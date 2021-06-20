f323276507824ca1550cfd59590040e557eb6212
studiousTribe <studioustribe@gmail.com>
2021-06-21 01:49:43 +0530

    added loader

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// show laoder
+const loading = () => {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+};
+
+//hide loader
+const complete = () => {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+};
+
 // get quotes from api
 const getQuotes = async () => {
+  loading();
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(apiUrl);
@@ -20,6 +34,7 @@ const getQuotes = async () => {
 
 // get new quotes randomly
 const newQuote = () => {
+  loading();
   let idx = Math.floor(Math.random() * apiQuotes.length);
   const quote = apiQuotes[idx];
   authorText.textContent = quote.author ?? 'Unknown';
@@ -27,6 +42,7 @@ const newQuote = () => {
     ? quoteText.classList.add('long-quote')
     : quoteText.classList.remove('long-quote');
   quoteText.textContent = quote.text;
+  complete();
 };
 
 // Tweet a quote
